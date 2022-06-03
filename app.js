2b1459ad34a97c5171d0b31d45f7ac3382196d1c
Kang <rkdguswns0803@gmail.com>
2022-06-03 14:49:18 +0900

    2D Context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.querySelector('#jsCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,24 +10,29 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event) {
   painting = true;
-  console.log(event);
-}
-
-function onMouseUp(event) {
-  stopPainting();
 }
 
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
-  canvas.addEventListener('mousedown', onMouseDown);
-  canvas.addEventListener('mouseup', onMouseUp);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
